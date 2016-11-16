f418450f573ae0af29be7ae65e8d1f24271b908a
ndyumin <nikita.dyumin@gmail.com>
2016-11-16 10:27:03 +0500

    add filter

--- observable.lisp
+++ observable.lisp
@@ -5,7 +5,9 @@
 (defun ones (next) 
 	(funcall next 1)
 	(funcall next 2)
-	(funcall next 3)) 
+	(funcall next 3)
+	(funcall next 4)
+	(funcall next 5)) 
 
 (funcall (observable #'ones) #'write) 
 
@@ -23,4 +25,21 @@
 (funcall 
 		(fmap 
 			(observable #'ones) #'dbl) 
+		#'write) 
+		
+(defun filter (obs pred)
+	(observable 
+		(lambda (next) 
+			(funcall obs
+				(lambda (val)
+					(when 
+						(funcall pred val) 
+						(funcall next val)))))))
+
+(defun even? (val)
+	(eq (rem val 2) 0))
+					
+(funcall 
+		(filter 
+			(observable #'ones) #'even?) 
 		#'write) 
